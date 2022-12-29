34956f3a40a0625cf66def5fc995c1ea0b584203
wirakora <wiraardi79@gmail.com>
2022-12-29 20:20:23 +0700

    init project

---
+++ src/main/java/com/domain/DemoRestapiApplication.java
@@ -0,0 +1,12 @@
+package com.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRestapiApplication.class, args);
+	}
+}
